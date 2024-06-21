d56f8c7f563222e50f82925145524bc073c20974
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2024-06-21 10:34:14 +0300

    finished CountLinesWordsAndCharacters

--- Java/Streams and Files/src/CountLinesWordsAndCharacters/CountLinesWordsAndCharacters.java
+++ Java/Streams and Files/src/CountLinesWordsAndCharacters/CountLinesWordsAndCharacters.java
@@ -1,22 +1,24 @@
 package CountLinesWordsAndCharacters;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class CountLinesWordsAndCharacters {
     public static void main(String[] args) {
 
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
-            String input = reader.readLine();
-            while(input != null){
-                String[] path = input.split("\\+");
-                String[] tokens = path[3].split(".");
-                input = reader.readLine();
-                //TODO finish the logic!
-            }
-        } catch (IOException e){
-            System.out.println(e.getMessage());
+        Scanner sc = new Scanner(System.in);
+        String filePath = sc.nextLine();
+        int indexOfDot = filePath.lastIndexOf('.');
+        int indexOfBackSlash = filePath.lastIndexOf('\\');
+
+        if(indexOfDot == -1){
+            System.out.println("The file doesn't have an extension");
         }
+
+        String fileName = filePath.substring(indexOfBackSlash + 1, indexOfDot);
+        String fileExtension = filePath.substring(indexOfDot + 1 );
+
+        System.out.println("File name: " + fileName);
+        System.out.println("File extension: " + fileExtension);
+        System.out.println("File size: {size} bytes");
     }
 }
