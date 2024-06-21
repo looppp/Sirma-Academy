03188c6dda9c683f778e738ab067120637065690
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2024-06-21 12:47:03 +0300

    CountUniqueWords allmost done
    Need to find the right REGEX

---
+++ Java/Streams and Files/src/CountUniqueWords/CountUniqueWords.java
@@ -0,0 +1,28 @@
+package CountUniqueWords;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class CountUniqueWords {
+    public static void main(String[] args) {
+        String filePath = "src\\CountUniqueWords\\input.txt";
+        HashSet<String> uniqueSetOfWords = new HashSet<>();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+            String nextLine = reader.readLine();
+
+            while (nextLine != null){
+                String[] inputWords = nextLine.split("\\s+|\\."); // TODO find the right regex
+                uniqueSetOfWords.addAll(Arrays.asList(inputWords));
+                nextLine = reader.readLine();
+            }
+
+            System.out.println(uniqueSetOfWords.size());
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
