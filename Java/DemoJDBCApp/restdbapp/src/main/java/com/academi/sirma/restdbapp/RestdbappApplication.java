55bbaefea41347031daddccf754fd465e5136f75
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2024-08-13 16:34:59 +0300

    JDBC and PostgreSQL App
    Createda a basic guiding structure for the application.

---
+++ Java/DemoJDBCApp/restdbapp/src/main/java/com/academi/sirma/restdbapp/RestdbappApplication.java
@@ -0,0 +1,13 @@
+package com.academi.sirma.restdbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestdbappApplication.class, args);
+	}
+
+}
---
+++ Java/DemoJDBCApp/restdbapp/src/test/java/com/academi/sirma/restdbapp/RestdbappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academi.sirma.restdbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestdbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
