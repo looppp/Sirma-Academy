a22f63b75471f28c99bf17c69c10ad09c1d2ee9b
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2024-09-07 13:53:25 +0300

    Initial commit for FootballApi

---
+++ Java/Java 2024 Final Project Assignment/football-api/src/main/java/com/sirma/footballapi/FootballApiApplication.java
@@ -0,0 +1,13 @@
+package com.sirma.footballapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballApiApplication.class, args);
+	}
+
+}
