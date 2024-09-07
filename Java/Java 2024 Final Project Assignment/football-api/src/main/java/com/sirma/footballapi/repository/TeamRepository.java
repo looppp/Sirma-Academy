1191110ac78cfd8c4c1e0656c66f7a07d3bb5eab
Miroslav Iliev <31375881+looppp@users.noreply.github.com>
2024-09-07 18:13:26 +0300

    Implemented the repositories

---
+++ Java/Java 2024 Final Project Assignment/football-api/src/main/java/com/sirma/footballapi/repository/TeamRepository.java
@@ -0,0 +1,9 @@
+package com.sirma.footballapi.repository;
+
+import com.sirma.footballapi.models.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
